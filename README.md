990615f26e9db3a9b91ca6df8d387688ceb56aa9
Martin Kondor <martinkondor@gmail.com>
2020-03-05 14:38:45 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# LuxDB
+# ☀ LuxDB
+
+[![version](https://img.shields.io/badge/version-v0.1.0-red.svg)](https://github.com/MartinKondor/LuxDB) [![Project Status](https://img.shields.io/badge/status-active-brightgreen.svg)](https://github.com/MartinKondor/SimpleComposer) ![Contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg) [![GitHub Issues](https://img.shields.io/github/issues/MartinKondor/LuxDB.svg)](https://github.com/MartinKondor/LuxDB/issues) ![Size](https://img.shields.io/bundlephobia/minzip/stormdb?color=brightgreen) [![License](https://img.shields.io/badge/license-MIT-blue)](./LICENSE)
+
 ☀ LuxDB is a tiny, lightweight, 0 dependency, easy-to-use JSON-based database
+
+```js
+const luxdb = new LuxDB('cache/testdb.json');
+luxdb.set('users', []);
+luxdb.point('users');
+luxdb.push({'id': 0, 'name': 'Joe Doe'})
+console.log(luxdb.get({'id': 0}));
+// Prints { id: 0, name: 'Joe Doe' }
+```
+
+## Examples
+
+```js
+const luxdb = new LuxDB('cache/testdb.json');
+luxdb.set('text', 'Some data')
+    .set('number', 7)
+    .set('array', [])
+    .set('object', {})
+    .save();
+```
+
+## Contributing
+
+Ways to contribute:
+
+* Check for open issues
+* Read the ```TODO``` file
+* Make an improvement
+
+### Steps
+
+1. Fork this repository
+2. Create a new branch (optional)
+3. Clone it
+4. Make your changes
+5. Upload them
+6. Make a pull request here
+
+## Authors
+
+* **[Martin Kondor](https://github.com/MartinKondor)**
+
+<p align="center">
+<a title="Fiverr" href="https://www.fiverr.com/martinkondor">
+<img id="fiverr-img" class="img-responsive" alt="Hire me on fiverr!" title="Hire me on fiverr!" src="https://martinkondor.github.io/img/hire_me_on_fiverr_button.png" width="222">
+</a>
+</p>
+
+<p align="center"><a href="https://www.patreon.com/bePatron?u=17006186" data-patreon-widget-type="become-patron-button"><img width="222" class="img-responsive" alt="Become a Patron!" title="Become a Patron!" src="https://martinkondor.github.io/img/become_a_patron_button.png"></a></p>
+
+## License
+
+Copyright &copy; Martin Kondor 2020.
+
+MIT license, see the [LICENSE](./LICENSE) file for more details.
---
+++ src/luxdb.js
@@ -0,0 +1,129 @@
+const fs = require('fs');
+
+
+const debug = (a='') => 
+{
+    console.log('LuxDB: ' + a);
+}
+
+
+/**
+ * The main API class for users.
+ */
+class LuxDB 
+{
+    /**
+     * 
+     * @param {string} filename Database main file name,
+     * can be non existent file
+     */
+    constructor(filename=false)
+    {
+        // If there is no filename, or
+        // if it's undefined
+        if (typeof filename !== 'string')
+        {
+            filename = 'luxdb.json';
+        }
+
+        // Pass filename to the engine
+        if (!fs.existsSync(filename))
+        {
+            // Create file
+            fs.appendFile(filename, '{}', (err) => {
+                if (err) throw err;
+                debug('Database created (' + filename + ') ...');
+            }); 
+        }
+        
+        // Load in database from file
+        let db = {};
+
+        fs.readFile(filename, (err, data) =>
+        {
+            if (err) throw err; 
+            if (!data) data = '{}';
+
+            db = JSON.parse(data);
+        });
+
+        this.dbFilename = filename;
+        this.db = db;
+        this.lastPointer = this.db;
+        this.pointer = this.db;
+    }
+
+    /**
+     * Sets the point value to the selected value
+     * 
+     * @param {string} where Of key or expresson from
+     * the below list
+     */
+    point(where)
+    {
+        if (where == '.')  // Set back to base
+        {
+            this.pointer = this.db;
+        }
+        else if (where == '..')  // Go up with one key
+        {
+            this.pointer = this.lastPointer;
+        }
+        else
+        {
+            this.pointer = this.pointer[where];
+        }
+        return this;
+    }
+
+    /**
+     * Function for quering from DB
+     */
+    get(key)
+    {
+        // Find object based on key
+        if (typeof key === 'object')
+        {
+            return this.pointer.find((element) => 
+            {
+                let keys = Object.keys(key);
+                for (let k in keys)
+                {
+                    if (element[k] !== key[k])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            });
+        }
+
+        return this.pointer[key];
+    }
+
+    /**
+     * Function for setting and attribute in DB
+     */
+    set(key, value)
+    {
+        this.pointer[key] = value;
+        return this;
+    }
+
+    /**
+     * 
+     * @param {object} obj Add the given obj to the current point array
+     */
+    push(obj)
+    {
+        this.pointer.push(obj);
+    }
+
+    /**
+     * Function for saving DB to file
+     */
+    save()
+    {
+        fs.writeFileSync(this.dbFilename, JSON.stringify(this.db));
+    }
+}
